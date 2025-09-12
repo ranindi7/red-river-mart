736a379d37d672ac24a14fdd08a883d11b4a1fb5
Ranindi Gunasekera <ngunasekera3@academic.rrc.ca>
2025-09-12 11:18:01 -0500

    Added htmlFor and button type to prevent Axe DevTool errors

--- Red River Mart/src/components/loginComponent/signInComponent.tsx
+++ Red River Mart/src/components/loginComponent/signInComponent.tsx
@@ -4,15 +4,15 @@ export default function SignIn() {
             <form>
                 <h1>Sign In</h1>
                 <div className="emailContainer">
-                    <label>Email: </label>
+                    <label htmlFor="email">Email: </label>
                     <input type="email"></input>
                 </div>
                 <div className="passwordContainer">
-                    <label>Password: </label>
+                    <label htmlFor="password">Password: </label>
                     <input type="password"></input>
                 </div>
                 <div className="loginButtonContainer">
-                    <button>Sign In</button>
+                    <button type="submit">Sign In</button>
                 </div>
             </form>
         </section>
